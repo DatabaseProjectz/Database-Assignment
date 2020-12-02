a4c28fd1f9715f345f63857b7c88baf255d083fa
NeranjiSulakshika <vpnsulakshika@students.nsbm.lk>
2020-12-02 18:59:16 +0530

    Updated

--- DatabaseManagementSystem/Form2.Designer.cs
+++ DatabaseManagementSystem/Form2.Designer.cs
@@ -29,25 +29,140 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblBookIssue = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.imgStudent = new System.Windows.Forms.PictureBox();
+            this.lblISBNno = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgStudent)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(496, 646);
+            this.panel1.Size = new System.Drawing.Size(831, 554);
             this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblBookIssue);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(831, 73);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblBookIssue
+            // 
+            this.lblBookIssue.AutoSize = true;
+            this.lblBookIssue.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookIssue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblBookIssue.Location = new System.Drawing.Point(306, 21);
+            this.lblBookIssue.Name = "lblBookIssue";
+            this.lblBookIssue.Size = new System.Drawing.Size(180, 31);
+            this.lblBookIssue.TabIndex = 0;
+            this.lblBookIssue.Text = "ISSUE BOOK";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.lblISBNno);
+            this.panel3.Controls.Add(this.imgStudent);
+            this.panel3.Location = new System.Drawing.Point(0, 73);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(401, 481);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(401, 73);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(430, 481);
+            this.panel4.TabIndex = 2;
+            // 
+            // imgStudent
+            // 
+            this.imgStudent.Location = new System.Drawing.Point(134, 65);
+            this.imgStudent.Name = "imgStudent";
+            this.imgStudent.Size = new System.Drawing.Size(124, 114);
+            this.imgStudent.TabIndex = 0;
+            this.imgStudent.TabStop = false;
+            // 
+            // lblISBNno
+            // 
+            this.lblISBNno.AutoSize = true;
+            this.lblISBNno.ForeColor = System.Drawing.Color.White;
+            this.lblISBNno.Location = new System.Drawing.Point(114, 247);
+            this.lblISBNno.Name = "lblISBNno";
+            this.lblISBNno.Size = new System.Drawing.Size(54, 13);
+            this.lblISBNno.TabIndex = 1;
+            this.lblISBNno.Text = "ISBN NO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 244);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(134, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SEARCH";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(496, 554);
+            this.ClientSize = new System.Drawing.Size(831, 554);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form2";
             this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgStudent)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +170,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblBookIssue;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblISBNno;
+        private System.Windows.Forms.PictureBox imgStudent;
     }
 }
--- DatabaseManagementSystem/HOME.Designer.cs
+++ DatabaseManagementSystem/HOME.Designer.cs
@@ -45,7 +45,7 @@
             this.panel1.ForeColor = System.Drawing.Color.White;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1264, 646);
+            this.panel1.Size = new System.Drawing.Size(1367, 788);
             this.panel1.TabIndex = 0;
             // 
             // button1

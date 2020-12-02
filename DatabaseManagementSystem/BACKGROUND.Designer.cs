ecad095ae1195fe983341b767897cd462fccd0f0
NeranjiSulakshika <vpnsulakshika@students.nsbm.lk>
2020-12-02 18:39:02 +0530

    Updated

---
+++ DatabaseManagementSystem/BACKGROUND.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DatabaseManagementSystem
+{
+    partial class BACKGROUND
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BACKGROUND));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnCompleteBookDetails = new System.Windows.Forms.Button();
+            this.btnReturnBooks = new System.Windows.Forms.Button();
+            this.btnIssueBooks = new System.Windows.Forms.Button();
+            this.btnStudent = new System.Windows.Forms.Button();
+            this.btnBooks = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1367, 788);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel2.Controls.Add(this.btnExit);
+            this.panel2.Controls.Add(this.btnAdmin);
+            this.panel2.Controls.Add(this.btnCompleteBookDetails);
+            this.panel2.Controls.Add(this.btnReturnBooks);
+            this.panel2.Controls.Add(this.btnIssueBooks);
+            this.panel2.Controls.Add(this.btnStudent);
+            this.panel2.Controls.Add(this.btnBooks);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1367, 72);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(1259, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(108, 72);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdmin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.ForeColor = System.Drawing.Color.White;
+            this.btnAdmin.Location = new System.Drawing.Point(1095, 0);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(158, 72);
+            this.btnAdmin.TabIndex = 5;
+            this.btnAdmin.Text = "ADMIN";
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnCompleteBookDetails
+            // 
+            this.btnCompleteBookDetails.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCompleteBookDetails.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCompleteBookDetails.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompleteBookDetails.ForeColor = System.Drawing.Color.White;
+            this.btnCompleteBookDetails.Location = new System.Drawing.Point(876, 0);
+            this.btnCompleteBookDetails.Name = "btnCompleteBookDetails";
+            this.btnCompleteBookDetails.Size = new System.Drawing.Size(213, 72);
+            this.btnCompleteBookDetails.TabIndex = 4;
+            this.btnCompleteBookDetails.Text = "COMPLETE BOOK DETAILS";
+            this.btnCompleteBookDetails.UseVisualStyleBackColor = false;
+            // 
+            // btnReturnBooks
+            // 
+            this.btnReturnBooks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnReturnBooks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReturnBooks.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturnBooks.ForeColor = System.Drawing.Color.White;
+            this.btnReturnBooks.Location = new System.Drawing.Point(657, 0);
+            this.btnReturnBooks.Name = "btnReturnBooks";
+            this.btnReturnBooks.Size = new System.Drawing.Size(213, 72);
+            this.btnReturnBooks.TabIndex = 3;
+            this.btnReturnBooks.Text = "RETURN BOOKS";
+            this.btnReturnBooks.UseVisualStyleBackColor = false;
+            // 
+            // btnIssueBooks
+            // 
+            this.btnIssueBooks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnIssueBooks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIssueBooks.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIssueBooks.ForeColor = System.Drawing.Color.White;
+            this.btnIssueBooks.Location = new System.Drawing.Point(438, 0);
+            this.btnIssueBooks.Name = "btnIssueBooks";
+            this.btnIssueBooks.Size = new System.Drawing.Size(213, 72);
+            this.btnIssueBooks.TabIndex = 2;
+            this.btnIssueBooks.Text = "ISSUE BOOKS";
+            this.btnIssueBooks.UseVisualStyleBackColor = false;
+            // 
+            // btnStudent
+            // 
+            this.btnStudent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnStudent.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStudent.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudent.ForeColor = System.Drawing.Color.White;
+            this.btnStudent.Location = new System.Drawing.Point(219, 0);
+            this.btnStudent.Name = "btnStudent";
+            this.btnStudent.Size = new System.Drawing.Size(213, 72);
+            this.btnStudent.TabIndex = 1;
+            this.btnStudent.Text = "STUDENT";
+            this.btnStudent.UseVisualStyleBackColor = false;
+            // 
+            // btnBooks
+            // 
+            this.btnBooks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnBooks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBooks.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBooks.ForeColor = System.Drawing.Color.White;
+            this.btnBooks.Location = new System.Drawing.Point(0, 0);
+            this.btnBooks.Name = "btnBooks";
+            this.btnBooks.Size = new System.Drawing.Size(213, 72);
+            this.btnBooks.TabIndex = 0;
+            this.btnBooks.Text = "BOOKS";
+            this.btnBooks.UseVisualStyleBackColor = false;
+            // 
+            // BACKGROUND
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1367, 788);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BACKGROUND";
+            this.Text = "Welcome";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnCompleteBookDetails;
+        private System.Windows.Forms.Button btnReturnBooks;
+        private System.Windows.Forms.Button btnIssueBooks;
+        private System.Windows.Forms.Button btnStudent;
+        private System.Windows.Forms.Button btnBooks;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+

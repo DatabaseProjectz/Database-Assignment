1f2725f74498109c6f1f2accc091b2db968cf90e
NeranjiSulakshika <vpnsulakshika@students.nsbm.lk>
2020-12-02 23:08:43 +0530

    Updated

--- DatabaseManagementSystem/ISSUE.Designer.cs
+++ DatabaseManagementSystem/ISSUE.Designer.cs
@@ -31,6 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ISSUE));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pnlRight = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBookName = new System.Windows.Forms.ComboBox();
             this.tdpBookIssue = new System.Windows.Forms.DateTimePicker();
             this.btnIssue = new System.Windows.Forms.Button();
             this.lblBookIssueDate = new System.Windows.Forms.Label();
@@ -52,10 +54,8 @@
             this.lblIsbnNo = new System.Windows.Forms.Label();
             this.imgStudent = new System.Windows.Forms.PictureBox();
             this.pnlTop = new System.Windows.Forms.Panel();
-            this.lblBookIssue = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.cmbBookName = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblBookIssue = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.pnlRight.SuspendLayout();
             this.pnlLeft.SuspendLayout();
@@ -100,11 +100,38 @@
             this.pnlRight.Size = new System.Drawing.Size(430, 481);
             this.pnlRight.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(50, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 16);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Maximum 3 books can be issued to one student.";
+            // 
+            // cmbBookName
+            // 
+            this.cmbBookName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBookName.FormattingEnabled = true;
+            this.cmbBookName.Items.AddRange(new object[] {
+            "Database Management Studies",
+            "Java",
+            "C#",
+            "C language",
+            "Introduction to Computer Science"});
+            this.cmbBookName.Location = new System.Drawing.Point(182, 279);
+            this.cmbBookName.Name = "cmbBookName";
+            this.cmbBookName.Size = new System.Drawing.Size(220, 24);
+            this.cmbBookName.TabIndex = 19;
+            // 
             // tdpBookIssue
             // 
-            this.tdpBookIssue.Location = new System.Drawing.Point(221, 336);
+            this.tdpBookIssue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tdpBookIssue.Location = new System.Drawing.Point(182, 320);
             this.tdpBookIssue.Name = "tdpBookIssue";
-            this.tdpBookIssue.Size = new System.Drawing.Size(181, 20);
+            this.tdpBookIssue.Size = new System.Drawing.Size(220, 22);
             this.tdpBookIssue.TabIndex = 18;
             // 
             // btnIssue
@@ -114,7 +141,7 @@
             this.btnIssue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnIssue.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIssue.ForeColor = System.Drawing.Color.White;
-            this.btnIssue.Location = new System.Drawing.Point(163, 404);
+            this.btnIssue.Location = new System.Drawing.Point(163, 384);
             this.btnIssue.Name = "btnIssue";
             this.btnIssue.Size = new System.Drawing.Size(124, 40);
             this.btnIssue.TabIndex = 17;
@@ -124,107 +151,119 @@
             // lblBookIssueDate
             // 
             this.lblBookIssueDate.AutoSize = true;
-            this.lblBookIssueDate.Location = new System.Drawing.Point(50, 339);
+            this.lblBookIssueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookIssueDate.Location = new System.Drawing.Point(34, 321);
             this.lblBookIssueDate.Name = "lblBookIssueDate";
-            this.lblBookIssueDate.Size = new System.Drawing.Size(110, 13);
+            this.lblBookIssueDate.Size = new System.Drawing.Size(131, 16);
             this.lblBookIssueDate.TabIndex = 14;
             this.lblBookIssueDate.Text = "Book Issue Date      : ";
             // 
             // lblBookName
             // 
             this.lblBookName.AutoSize = true;
-            this.lblBookName.Location = new System.Drawing.Point(50, 300);
+            this.lblBookName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookName.Location = new System.Drawing.Point(34, 282);
             this.lblBookName.Name = "lblBookName";
-            this.lblBookName.Size = new System.Drawing.Size(105, 13);
+            this.lblBookName.Size = new System.Drawing.Size(128, 16);
             this.lblBookName.TabIndex = 13;
-            this.lblBookName.Text = "Book Name             :";
+            this.lblBookName.Text = "Book Name               :";
             // 
             // txtStudentEmail
             // 
-            this.txtStudentEmail.Location = new System.Drawing.Point(221, 260);
+            this.txtStudentEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentEmail.Location = new System.Drawing.Point(182, 242);
             this.txtStudentEmail.Name = "txtStudentEmail";
-            this.txtStudentEmail.Size = new System.Drawing.Size(181, 20);
+            this.txtStudentEmail.Size = new System.Drawing.Size(220, 22);
             this.txtStudentEmail.TabIndex = 12;
             // 
             // txtStudentContactNo
             // 
-            this.txtStudentContactNo.Location = new System.Drawing.Point(221, 224);
+            this.txtStudentContactNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentContactNo.Location = new System.Drawing.Point(182, 205);
             this.txtStudentContactNo.Name = "txtStudentContactNo";
-            this.txtStudentContactNo.Size = new System.Drawing.Size(181, 20);
+            this.txtStudentContactNo.Size = new System.Drawing.Size(220, 22);
             this.txtStudentContactNo.TabIndex = 11;
             // 
             // txtStudentBatch
             // 
-            this.txtStudentBatch.Location = new System.Drawing.Point(221, 185);
+            this.txtStudentBatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentBatch.Location = new System.Drawing.Point(182, 168);
             this.txtStudentBatch.Name = "txtStudentBatch";
-            this.txtStudentBatch.Size = new System.Drawing.Size(181, 20);
+            this.txtStudentBatch.Size = new System.Drawing.Size(220, 22);
             this.txtStudentBatch.TabIndex = 10;
             // 
             // txtFaculty
             // 
-            this.txtFaculty.Location = new System.Drawing.Point(221, 144);
+            this.txtFaculty.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFaculty.Location = new System.Drawing.Point(182, 130);
             this.txtFaculty.Name = "txtFaculty";
-            this.txtFaculty.Size = new System.Drawing.Size(181, 20);
+            this.txtFaculty.Size = new System.Drawing.Size(220, 22);
             this.txtFaculty.TabIndex = 9;
             // 
             // txtStudentName
             // 
-            this.txtStudentName.Location = new System.Drawing.Point(221, 106);
+            this.txtStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentName.Location = new System.Drawing.Point(182, 92);
             this.txtStudentName.Name = "txtStudentName";
-            this.txtStudentName.Size = new System.Drawing.Size(181, 20);
+            this.txtStudentName.Size = new System.Drawing.Size(220, 22);
             this.txtStudentName.TabIndex = 8;
             // 
             // lblStudentEmail
             // 
             this.lblStudentEmail.AutoSize = true;
-            this.lblStudentEmail.Location = new System.Drawing.Point(50, 267);
+            this.lblStudentEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentEmail.Location = new System.Drawing.Point(34, 245);
             this.lblStudentEmail.Name = "lblStudentEmail";
-            this.lblStudentEmail.Size = new System.Drawing.Size(105, 13);
+            this.lblStudentEmail.Size = new System.Drawing.Size(129, 16);
             this.lblStudentEmail.TabIndex = 7;
-            this.lblStudentEmail.Text = "Student Email          :";
+            this.lblStudentEmail.Text = "Student Email            :";
             // 
             // lblStudentContactNo
             // 
             this.lblStudentContactNo.AutoSize = true;
-            this.lblStudentContactNo.Location = new System.Drawing.Point(50, 231);
+            this.lblStudentContactNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentContactNo.Location = new System.Drawing.Point(34, 208);
             this.lblStudentContactNo.Name = "lblStudentContactNo";
-            this.lblStudentContactNo.Size = new System.Drawing.Size(104, 13);
+            this.lblStudentContactNo.Size = new System.Drawing.Size(128, 16);
             this.lblStudentContactNo.TabIndex = 6;
-            this.lblStudentContactNo.Text = "Student Contact No:";
+            this.lblStudentContactNo.Text = "Student Contact No :";
             // 
             // lblStudentBatch
             // 
             this.lblStudentBatch.AutoSize = true;
-            this.lblStudentBatch.Location = new System.Drawing.Point(50, 192);
+            this.lblStudentBatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentBatch.Location = new System.Drawing.Point(34, 171);
             this.lblStudentBatch.Name = "lblStudentBatch";
-            this.lblStudentBatch.Size = new System.Drawing.Size(105, 13);
+            this.lblStudentBatch.Size = new System.Drawing.Size(126, 16);
             this.lblStudentBatch.TabIndex = 5;
-            this.lblStudentBatch.Text = "Student Batch         :";
+            this.lblStudentBatch.Text = "Student Batch           :";
             // 
             // lblFaculty
             // 
             this.lblFaculty.AutoSize = true;
-            this.lblFaculty.Location = new System.Drawing.Point(50, 151);
+            this.lblFaculty.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFaculty.Location = new System.Drawing.Point(34, 133);
             this.lblFaculty.Name = "lblFaculty";
-            this.lblFaculty.Size = new System.Drawing.Size(104, 13);
+            this.lblFaculty.Size = new System.Drawing.Size(126, 16);
             this.lblFaculty.TabIndex = 4;
-            this.lblFaculty.Text = "Faculty                    :";
+            this.lblFaculty.Text = "Faculty                        :";
             // 
             // lblStudentName
             // 
             this.lblStudentName.AutoSize = true;
-            this.lblStudentName.Location = new System.Drawing.Point(50, 113);
+            this.lblStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentName.Location = new System.Drawing.Point(34, 95);
             this.lblStudentName.Name = "lblStudentName";
-            this.lblStudentName.Size = new System.Drawing.Size(102, 13);
+            this.lblStudentName.Size = new System.Drawing.Size(126, 16);
             this.lblStudentName.TabIndex = 3;
-            this.lblStudentName.Text = "Student Name        :";
+            this.lblStudentName.Text = "Student Name          :";
             // 
             // lblDetails
             // 
             this.lblDetails.AutoSize = true;
             this.lblDetails.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDetails.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.lblDetails.Location = new System.Drawing.Point(158, 16);
+            this.lblDetails.Location = new System.Drawing.Point(158, 19);
             this.lblDetails.Name = "lblDetails";
             this.lblDetails.Size = new System.Drawing.Size(113, 26);
             this.lblDetails.TabIndex = 2;
@@ -255,6 +294,7 @@
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Text = "SEARCH";
             this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // txtIsbnNo
             // 
@@ -287,22 +327,11 @@
             // 
             this.pnlTop.Controls.Add(this.button1);
             this.pnlTop.Controls.Add(this.lblBookIssue);
-            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlTop.Location = new System.Drawing.Point(0, 0);
             this.pnlTop.Name = "pnlTop";
-            this.pnlTop.Size = new System.Drawing.Size(831, 73);
+            this.pnlTop.Size = new System.Drawing.Size(831, 554);
             this.pnlTop.TabIndex = 0;
-            // 
-            // lblBookIssue
-            // 
-            this.lblBookIssue.AutoSize = true;
-            this.lblBookIssue.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBookIssue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.lblBookIssue.Location = new System.Drawing.Point(312, 21);
-            this.lblBookIssue.Name = "lblBookIssue";
-            this.lblBookIssue.Size = new System.Drawing.Size(180, 31);
-            this.lblBookIssue.TabIndex = 0;
-            this.lblBookIssue.Text = "ISSUE BOOK";
             // 
             // button1
             // 
@@ -319,30 +348,16 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // cmbBookName
+            // lblBookIssue
             // 
-            this.cmbBookName.FormattingEnabled = true;
-            this.cmbBookName.Items.AddRange(new object[] {
-            "Database Management Studies",
-            "Java",
-            "C#",
-            "C language",
-            "Introduction to Computer Science"});
-            this.cmbBookName.Location = new System.Drawing.Point(221, 297);
-            this.cmbBookName.Name = "cmbBookName";
-            this.cmbBookName.Size = new System.Drawing.Size(181, 21);
-            this.cmbBookName.TabIndex = 19;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(50, 456);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(337, 16);
-            this.label1.TabIndex = 20;
-            this.label1.Text = "Maximum 3 books can be issued to one student.";
+            this.lblBookIssue.AutoSize = true;
+            this.lblBookIssue.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookIssue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblBookIssue.Location = new System.Drawing.Point(312, 21);
+            this.lblBookIssue.Name = "lblBookIssue";
+            this.lblBookIssue.Size = new System.Drawing.Size(180, 31);
+            this.lblBookIssue.TabIndex = 0;
+            this.lblBookIssue.Text = "ISSUE BOOK";
             // 
             // ISSUE
             // 
@@ -354,6 +369,7 @@
             this.Name = "ISSUE";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.ISSUE_Load);
             this.panel1.ResumeLayout(false);
             this.pnlRight.ResumeLayout(false);
             this.pnlRight.PerformLayout();
